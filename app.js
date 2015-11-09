2e99f283e5b787f4a8c3fc34659f62f41d7795b4
Fer Martin <fernandomartinsantana@gmail.com>
2015-11-09 16:51:23 +0800

    Added express routes

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
 var express = require('express');
 var app     = express();
 var port    = process.env.PORT || 3000;
+var router  = express.Router();
 
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
 // Middleware
-app.use(function(req, res, next) {
+router.use(function(req, res, next) {
   console.log('%s request to %s from %s', req.method, req.path, req.ip);
   next();
 });
 
-app.get('/', function(req, res) {
-    res.render('index');
+router.get('/', function(req, res) {
+  res.render('index', { header: 'index!'});
 });
+
+router.get('/contact', function(req, res) {
+  res.render('contact', { header: 'contact!'});
+});
+
+router.get('/about', function(req, res) {
+  res.render('about', { header: 'about!'});
+});
+
+app.use('/', router);
 
 app.listen(port);
 console.log('Server started on ' + port);
