1bfc2034e06b752206b0cf8555760540c89a761c
Fer Martin <fernandomartinsantana@gmail.com>
2015-11-09 15:36:31 +0800

    Added views

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ var express = require('express');
 var app     = express();
 var port    = process.env.PORT || 3000;
 
-app.get('/', function(req, res) {
-  res.send('Hello World');
-});
+app.set('views', './views');
+app.set('view engine', 'ejs');
 
+app.get('/', function(req, res) {
+    res.render('index');
+});
 
 app.listen(port);
 console.log('Server started on ' + port);
