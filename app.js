03075586b494315bae7b5861441c74760b989948
Fer Martin <fernandomartinsantana@gmail.com>
2015-11-09 15:38:57 +0800

    Added middleware-like function

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ var port    = process.env.PORT || 3000;
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
+// Middleware
+app.use(function(req, res, next) {
+  console.log('%s request to %s from %s', req.method, req.path, req.ip);
+  next();
+});
+
 app.get('/', function(req, res) {
     res.render('index');
 });
